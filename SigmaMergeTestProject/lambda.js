11e887c2dc0116045bd34029717237e01d40f3fa
sigmavcs <jchamath+sigma@gmail.com>
2019-07-01 10:51:44 +0530

    deleted second, modified lambda

--- SigmaMergeTestProject/lambda.js
+++ SigmaMergeTestProject/lambda.js
@@ -1,4 +1,6 @@
 exports.handler = function(event, context, callback) {
+
+    const number = 15;
     
-    callback(null, {"message": "Successfully executed"});
+    callback(null, {"message": "Successfully executed" + number});
 }
