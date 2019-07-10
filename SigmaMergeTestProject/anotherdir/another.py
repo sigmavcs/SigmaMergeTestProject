d2490c96db2687e8d01b69a7fde4540493139820
sigmavcs <jchamath+sigma@gmail.com>
2019-07-10 11:26:00 +0530

    dev <07/08 11:25>

--- SigmaMergeTestProject/anotherdir/another.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-a = np.arange(15).reshape(3, 5)
-print a
-
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
